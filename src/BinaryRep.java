3f27a009057461166902a473be29677f32f340f4
jgoodson2 <jonathongoodson@clayton.edu>
2017-04-20 13:58:40 -0400

    Add getBinaryString() utility method; finish main method logic

--- src/BinaryRep.java
+++ src/BinaryRep.java
@@ -4,9 +4,23 @@ public class BinaryRep {
 
     public static void main(String[] args) {
         int num;
+        String binString = "";
 
         num = getPositiveInt("Please enter a positive integer: ");
+        System.out.println("Binary representation = " + getBinaryString(num));
+    }
 
+    private static String getBinaryString(int num) {
+        String binRep = "";
+        char newChar;
+
+        do {
+            newChar = (num % 2 > 0) ? '1' : '0';
+            binRep = newChar + binRep;
+            num /= 2;
+        } while (num > 0);
+
+        return binRep;
     }
 
     private static int getPositiveInt(String prompt) {
