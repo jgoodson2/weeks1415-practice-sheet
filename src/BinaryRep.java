f22bad6c9f2dcf08961190b20166b3c233c79951
jgoodson2 <jonathongoodson@clayton.edu>
2017-04-20 13:30:43 -0400

    WIP: build getPositiveInt() utility method

--- src/BinaryRep.java
+++ src/BinaryRep.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 public class BinaryRep {
 
     public static void main(String[] args) {
-	// write your code here
+        int num;
+
+        num = getPositiveInt("Please enter a positive integer: ");
+
+    }
+
+    private static int getPositiveInt(String prompt) {
+        Scanner scan = new Scanner(System.in);
+        int input = 0;
+        boolean isValid = false;
+        do {
+            System.out.println(prompt);
+            if (!scan.hasNextInt()) {
+                System.out.println("Error: not an integer.");
+                scan.nextLine();//clear the scanner
+            } else {
+                input = scan.nextInt();
+                if (input <= 0) {
+                    System.out.println("Error: input is not positive.");
+                } else {
+                    isValid = true;
+                }
+            }
+        } while (!isValid);
+        return input;
     }
 }
