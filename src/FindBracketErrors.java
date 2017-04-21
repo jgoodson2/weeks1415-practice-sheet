22fdf82ca2b5d195d4e8dcfb53b0fd9863ea0dc0
jgoodson2 <jonathongoodson@clayton.edu>
2017-04-21 16:44:08 -0400

    WIP: add proof-of-concept logic to demostrate that I can track characters line-by-line, col-by-col

--- src/FindBracketErrors.java
+++ src/FindBracketErrors.java
@@ -11,6 +11,33 @@ public class FindBracketErrors {
         String filePath = new File("").getAbsolutePath() + "/" + fileName;
         String fileContents = getFileContents(filePath);
         System.out.println("fileContents = " + fileContents);
+        System.out.println("All opening and closing bracket counts match = " + allBracketCountsMatch(fileContents));
+        /////////
+        File myFile = new File(filePath);
+        Scanner inputFile;
+        int lineCount = 0;
+        String line;
+        int colCount;
+        try {
+            inputFile = new Scanner(myFile);
+            while (inputFile.hasNext()) {
+                //contents.append(inputFile.nextLine());
+                line = inputFile.nextLine();
+                //System.out.println("Line " + ++lineCount + ": " + line);
+                System.out.println("Line " + lineCount++ + ":");
+                colCount = 0;
+                for (char col : line.toCharArray()) {
+                    System.out.println("Col " + colCount++ + ": " + col);
+                }
+            }
+        } catch (FileNotFoundException fnf) {
+            System.out.println(fnf.getMessage());
+        }
+
+
+    }
+
+    private static boolean allBracketCountsMatch(String st) {
 
         char[] openers = {'(', '{', '['};
         char[] closers = {')', '}', ']'};
@@ -18,14 +45,14 @@ public class FindBracketErrors {
         int[] closerCount = new int[3];
 
         for (int i = 0; i < openers.length; ++i) {
-            for (char ch : fileContents.toCharArray()) {
+            for (char ch : st.toCharArray()) {
                 if (ch == openers[i]) {
                     openerCount[i]++;
                 }
             }
             System.out.println("Number of \'" + openers[i] + "\' = " + openerCount[i]);
 
-            for (char ch : fileContents.toCharArray()) {
+            for (char ch : st.toCharArray()) {
                 if (ch == closers[i]) {
                     closerCount[i]++;
                 }
@@ -33,6 +60,12 @@ public class FindBracketErrors {
             System.out.println("Number of \'" + closers[i] + "\' = " + closerCount[i]);
         }
 
+        for (int i = 0; i < openerCount.length; ++i) {
+            if (openerCount[i] != closerCount[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private static String getFileContents(String filePath) {
