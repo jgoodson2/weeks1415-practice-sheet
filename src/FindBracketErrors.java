12ebfde3adbf06e87f548cacdbd4a2c3a62c3dd0
jgoodson2 <jonathongoodson@clayton.edu>
2017-04-21 13:35:46 -0400

    Add code.txt to hold sample code; add basic logic to src/FindBracketErrors.java to read from code.txt

--- src/FindBracketErrors.java
+++ src/FindBracketErrors.java
@@ -1,8 +1,29 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.File;
+
 public class FindBracketErrors {
 
     public static void main(String[] args) {
 
-        System.out.println("Time to start coding");
-    }
+        StringBuilder fileContents = new StringBuilder();
+        String fileName = "code.txt";
+        String filePath = new File("").getAbsolutePath() + "/" + fileName;
+        File myFile = new File(filePath);
+        Scanner inputFile;
 
+        System.out.println("filePath = " + filePath);
+
+        try {
+            inputFile = new Scanner(myFile);
+
+            while (inputFile.hasNext()) {
+                fileContents.append(inputFile.nextLine());
+            }
+        } catch (FileNotFoundException fnf) {
+            //display total of 0
+            System.out.println(fnf.getMessage());
+        }
+        System.out.println("fileContents = " + fileContents);
+    }
 }
